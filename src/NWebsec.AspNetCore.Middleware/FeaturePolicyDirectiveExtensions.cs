c8e0c298a63d5f300eef31dd1f3487a1534fa3c9
Matthew McCorry <mccorry@gmail.com>
2019-04-07 00:15:29 +0100

    a

--- src/NWebsec.AspNetCore.Middleware/FeaturePolicyDirectiveExtensions.cs
+++ src/NWebsec.AspNetCore.Middleware/FeaturePolicyDirectiveExtensions.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Linq;
 using NWebsec.Core.Common.HttpHeaders.Configuration;
+using NWebsec.Core.Common.HttpHeaders.Csp;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.AspNetCore.Builder
@@ -11,6 +13,41 @@ namespace Microsoft.AspNetCore.Builder
             if (directive == null) throw new ArgumentNullException(nameof(directive));
 
             directive.NoneSrc = true;
+        }
+
+        public static T Self<T>(this T directive) where T : class, IFeaturePolicyDirectiveBasicConfiguration
+        {
+            if (directive == null) throw new ArgumentNullException(nameof(directive));
+
+            directive.SelfSrc = true;
+            return directive;
+        }
+
+        public static T Enabled<T>(this T directive) where T : class, IFeaturePolicyDirectiveBasicConfiguration
+        {
+            if (directive == null) throw new ArgumentNullException(nameof(directive));
+
+            directive.Enabled = true;
+            return directive;
+        }
+
+        public static T CustomSources<T>(this T directive, params string[] sources) where T : class, IFeaturePolicyDirectiveBasicConfiguration
+        {
+            if (directive == null) throw new ArgumentNullException(nameof(directive));
+            if (sources.Length == 0) throw new ArgumentException("You must supply at least one source.", nameof(sources));
+
+            try
+            {
+                directive.CustomSources = sources
+                    .Select(s => CspUriSource.Parse(s).ToString())
+                    .ToArray();
+            }
+            catch (InvalidCspSourceException e)
+            {
+                throw new ArgumentException("Invalid source. Details: " + e.Message, nameof(sources), e);
+            }
+
+            return directive;
         }
     }
 }
